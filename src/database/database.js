ce70786c0017985a579b82f68de6c9d51d82fd50
NoeliaVasquezCrespo <noelia.vasquez.crespo@gmail.com>
2025-06-17 21:03:34 -0400

    feat: agregar SSL

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,14 @@ export const sequelize = new Sequelize(
         host: config.DB_HOST, // db host
         dialect: config.DB_DIALECT, // db dialect
         logging: console.log, // disable logging; default: console.log
+
+        dialectOptions: config.DB_USE_SSL === 'true' ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // para asegurar que la conexión sea segura
+            },
+        } : {}, 
+        
     }
 
 );
